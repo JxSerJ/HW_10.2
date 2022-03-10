ebc22b221e918a6ad105e46ac760283bebe4ab77
S.P <sergey.a.protsenko@mail.ru>
2022-03-10 20:24:45 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask
+
+from utils import load_data, build_candidate_instances, build_root_page_data, build_skills_page_data, \
+    build_skill_id_page_data
+from config import DATA_FILE_PATH
+
+data = load_data(DATA_FILE_PATH)
+candidate_list, skill_list = build_candidate_instances(data)
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def page_index():
+    return "<H1>Главная страница</H1><br>" \
+           "<H2>Список кандидатов</H2><br>" \
+           f"{build_root_page_data(candidate_list)}<br>" \
+           f"<br><a href='../skill/'>Перейти на страницу навыков</a>"
+
+
+@application.route("/candidate/<candidate_id>")
+def page_candidate(candidate_id):
+
+    candidate_id = int(candidate_id) - 1
+
+    string = f'''
+<img src="{candidate_list[candidate_id].avatar}">
+<pre>
+  Имя кандидата - <b>{candidate_list[candidate_id].name}</b>
+  Позиция кандидата - <b>{candidate_list[candidate_id].position}</b>
+  Навыки: <b>{', '.join(candidate_list[candidate_id].skills)}</b><br>
+  Пол: <b>{candidate_list[candidate_id].gender}</b>
+  Возраст: <b>{candidate_list[candidate_id].age}</b>
+
+<br><a href='../'>Перейти на главную</a>
+</pre>
+'''
+    return string
+
+
+@application.route("/skill/")
+def page_skill():
+    return "<H1>Список навыков</H1><br>" \
+           "<H2>Выберете навык</H2><br>" \
+           f"{build_skills_page_data(skill_list)}" \
+           f"<br><a href='../'>Перейти на главную</a>"
+
+
+@application.route("/skill/<skill_id>")
+def page_skill_id(skill_id):
+
+    string = f'''<H1>Список кандидатов с навыком "{skill_id.capitalize()}"</H1><br>
+<pre>
+{build_skill_id_page_data(candidate_list, skill_id)}
+<br><a href='../'>Перейти на главную</a>
+<br><a href='../skill/'>Перейти на страницу навыков</a>
+</pre>
+'''
+    return string
+
+
+application.run()
---
+++ utils.py
@@ -0,0 +1,92 @@
+import json
+from classes import Candidate
+
+
+def load_data(path: str) -> list:
+    """
+    JSON Data loader
+
+    :param path: data-file path
+    :return: data
+    """
+    with open(path, 'r', encoding="UTF-8") as file:
+        data = json.load(file)
+        return data
+
+
+def build_candidate_instances(data: list) -> tuple[list, list]:
+    """
+    Object instance initializer (class Candidate)
+
+    :param data: list of dictionaries
+    :return: list of objects
+    """
+    candidate_list = []
+    skill_list = []
+
+    for i in range(len(data)):
+        candidate = Candidate(candidate_id=data[i]["id"], name=data[i]["name"], avatar=data[i]["picture"],
+                              position=data[i]["position"], gender=data[i]["gender"], age=data[i]["age"],
+                              skills=data[i]["skills"].lower().split(", "))
+        skill_list.extend(candidate.skills)
+        candidate_list.append(candidate)
+
+    skill_list = list(set(skill_list))
+
+    return candidate_list, skill_list
+
+
+def build_root_page_data(candidates: list) -> str:
+    """
+    Root page builder with candidates
+
+    :param candidates: list of instances of class Candidate
+    :return: string
+    """
+    string = ""
+
+    for i in candidates:
+        candidate_string = f"<b>Имя кандидата</b> - {i.name}<br>" \
+                           f"<b>Позиция</b> - {i.position}<br>" \
+                           f"<b>Навыки</b> - {', '.join(i.skills)}<br>" \
+                           f"<a href='/candidate/{i.candidate_id}'>Перейти на страницу кандидата</a><br><br><br>"
+        string += candidate_string
+
+    return string
+
+
+def build_skills_page_data(skill_list: list) -> str:
+    """
+    Page with all candidate skills builder
+
+    :param skill_list: list of skills
+    :return: string
+    """
+    string = ""
+
+    for i in skill_list:
+        skill_string = f"<a href='/skill/{i}'>{i.capitalize()}</a><br>"
+        string += skill_string
+
+    return string
+
+
+def build_skill_id_page_data(candidate_list: list, skill_id: str) -> str:
+    """
+    Skill page builder. Generates candidate list who has skill_id
+
+    :param skill_id: skill for resolve
+    :param candidate_list: list of Candidate instances
+    :return: string
+    """
+    string = ""
+
+    for i in candidate_list:
+        if skill_id in i.skills:
+            candidate_string = f"<b>Имя кандидата</b> - {i.name}<br>" \
+                               f"<b>Позиция</b> - {i.position}<br>" \
+                               f"<b>Навыки</b> - {', '.join(i.skills)}<br>" \
+                               f"<a href='/candidate/{i.candidate_id}'>Перейти на страницу кандидата</a><br><br><br>"
+            string += candidate_string
+
+    return string
